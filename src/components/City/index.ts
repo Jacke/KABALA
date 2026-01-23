860e776e181f316f888011418d9139471776b656
Stan Sobolev <iamjacke@gmail.com>
2026-01-23 15:30:50 +0300

    feat(04-01): add barrel export for City components

---
+++ src/components/City/index.ts
@@ -0,0 +1,10 @@
+/**
+ * City components barrel export.
+ * Re-exports all components used in city detail pages.
+ */
+
+export { MetricCard } from './MetricCard';
+export { MetricSection } from './MetricSection';
+
+export type { MetricCardProps } from './MetricCard';
+export type { MetricSectionProps } from './MetricSection';
